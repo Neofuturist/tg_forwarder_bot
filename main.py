4c1a5f86bd33c692e50867fa2c0fdbaca2f2f375
Neofuturist <e.derevlev@yandex.ru>
2023-05-18 01:10:30 +0300

    temp

--- main.py
+++ main.py
@@ -3,11 +3,10 @@ import os
 from dotenv import load_dotenv
 import telebot
 
-
 load_dotenv()
 tgToken = os.environ.get("TELEGRAM_API_TOKEN")
-id_from = os.environ.get("FROM_USER_ID")
-id_to = os.environ.get("TO_USER_ID")
+id_from = int(os.environ.get("FROM_USER_ID"))
+id_to = int(os.environ.get("TO_USER_ID"))
 bot = telebot.TeleBot(tgToken)
 
 
@@ -15,13 +14,11 @@ bot = telebot.TeleBot(tgToken)
 def get_text_messages(message):
     print('message')
     try:
-        if str(message.from_user.id) == id_from:
+        if message.from_user.id == id_from:
             bot.send_message(id_to, str(message.text))
     except Exception as err:
         print('Ошибка:', err)
-        print('Трейсбэк:\n', traceback.format_exc())
+        print('Описание:\n', traceback.format_exc())
 
 
 bot.polling(none_stop=True, interval=0)
-
-
