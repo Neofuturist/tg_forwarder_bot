0b86efb755686a747bd5fc8c3ab4eea71ff84da0
Neofuturist <e.derevlev@yandex.ru>
2023-05-18 00:32:43 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import traceback
+import os
+from dotenv import load_dotenv
+import telebot
+
+
+load_dotenv()
+tgToken = os.environ.get("TELEGRAM_API_TOKEN")
+id_from = os.environ.get("FROM_USER_ID")
+id_to = os.environ.get("TO_USER_ID")
+bot = telebot.TeleBot(tgToken)
+
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    print('message')
+    try:
+        if str(message.from_user.id) == id_from:
+            bot.send_message(id_to, str(message.text))
+    except Exception as err:
+        print('Ошибка:', err)
+        print('Трейсбэк:\n', traceback.format_exc())
+
+
+bot.polling(none_stop=True, interval=0)
+
+
